c44dba9f59345cd7ea8089d8ecb9f8cac7501d0c
Oliver Anteros <rift95@live.se>
2017-07-24 23:35:29 +0200

    tried to fix broken cdn

--- src/turingjs.ts
+++ src/turingjs.ts
@@ -1,3 +1,7 @@
 import { Language as _Language } from './language';
 
-export class  Language<T> extends _Language<T> {};
+export class Language<T> extends _Language<T> {};
+
+if (window) {
+    window['Language'] = Language;
+}
