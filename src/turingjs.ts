37c47296d58d087224d00dd0e3d1be1e4ef60905
Oliver Anteros <rift95@live.se>
2017-08-23 23:48:14 +0200

    ..

--- src/turingjs.ts
+++ src/turingjs.ts
@@ -3,5 +3,8 @@ import { Language as _Language } from './language';
 export class Language<T> extends _Language<T> {};
 
 if (typeof window !== 'undefined') {
+    // Setup for running in the browser
+    // TODO: make sure this actually works...
     window['Language'] = Language;
-}   
+}
+
